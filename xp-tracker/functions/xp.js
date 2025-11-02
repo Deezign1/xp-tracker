41c1c4f8b416bf09b68914f270729fff286ca1b5
Deezign1 <stevcpps@gmail.com>
2025-11-02 12:02:20 -0500

    Delete xp-tracker directory

--- xp-tracker/functions/xp.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  try {
-    const res = await fetch("https://wort.gg/api/stats/FieyerFox/multiplayer");
-    const data = await res.json();
-    return Response.json({ total_xp: data.xp.total });
-  } catch (e) {
-    return Response.json({ error: e.message }, { status: 500 });
-  }
-}
